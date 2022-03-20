23b1b58087257375b5200a31c0cad8300275ac65
agholor-christotle <agholorchristotle@gmail.com>
2022-03-20 01:58:36 -0700

    folder containing all pages for navigation at the dashboard

---
+++ pages/_app.js
@@ -0,0 +1,37 @@
+import '../styles/globals.css'
+import Link from 'next/link'
+
+function Marketplace({ Component, pageProps }) {
+  return (
+    <div>
+      <nav className="border-b p-6">
+        <p className="text-4xl font-bold">Christotle NFT Marketplace</p>
+        <div className="flex mt-4">
+          <Link href="/">
+            <a className="mr-4 text-pink-500">
+              NFTs Market
+            </a>
+          </Link>
+          <Link href="/create-item">
+            <a className="mr-6 text-pink-500">
+              Sell NFTs
+            </a>
+          </Link>
+          <Link href="/my-assets">
+            <a className="mr-6 text-pink-500">
+              My NFTs
+            </a>
+          </Link>
+          <Link href="/creator-dashboard">
+            <a className="mr-6 text-pink-500">
+              My Dashboard
+            </a>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
+}
+
+export default Marketplace
